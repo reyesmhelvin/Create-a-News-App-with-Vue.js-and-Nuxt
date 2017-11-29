8e69cac2c0e91027cf13adc6193fae467b7a0b0f
Mhelvin Reyes <mhelvinreyes26@gmail.com>
2017-11-29 13:06:41 +0800

    Build Global Navigation with Layouts and nuxt-link in Nuxt

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <header>
+            <nav class="flex w-75 justify-around f4 code">
+                <nuxt-link to="/">Home</nuxt-link>
+                <nuxt-link to="/new">New</nuxt-link>
+                <nuxt-link to="/ask">Ask</nuxt-link>
+            </nav>
+        </header>
+        <nuxt></nuxt>
+    </div>
+</template>
