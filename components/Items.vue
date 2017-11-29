f9a770bd7a7c7f8fd126bd152b60435c3defe87b
Mhelvin Reyes <mhelvinreyes26@gmail.com>
2017-11-29 16:02:36 +0800

    Display Only Available Data in Vue.js and Nuxt

--- components/Items.vue
+++ components/Items.vue
@@ -1,19 +1,26 @@
 <template>
   <div class="code">
     <ul>
-      <li class="item" v-for="item in items" :key="item.id">
+      <li class="item f6" v-for="item in items" :key="item.id">
         <div class="score">
           {{item.score}}
         </div>
         <div class="title">
           {{item.title}}
+          <template v-if="item.url">
+            <br>
+            <a class="f7" :href="item.url">{{item.url | hostname}}</a>
+          </template>
         </div>
         <div class="details">
-          by {{item.by}} {{item.time | timeSince}} ago
-        </div>
-        <div class="comments">
-          {{item.descendants}} comments
-        </div>
+          by {{item.by}} 
+          <p class="ma0 i f7">{{item.time | timeSince}} ago</p>
+        </div>   
+        <template v-if="item.descendants">
+          <div class="comments">
+            {{item.descendants}} comments
+          </div>
+        </template>
       </li>
     </ul>
   </div>
