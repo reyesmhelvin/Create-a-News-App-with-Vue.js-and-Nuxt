86f058d9c2fa3d1d53ed3cee279db58891877dfc
Mhelvin Reyes <mhelvinreyes26@gmail.com>
2017-11-29 12:46:54 +0800

    Access the Router Params to Load Page-Specific Data in Vue.js and Nuxt

---
+++ components/Items.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="code">
+    <ul>
+      <li v-for="item in items" :key="item.id">{{item.title}}</li>
+    </ul>
+  </div>
+</template>
+<script>
+  import {mapState} from "vuex"
+
+  export default {
+    computed: mapState([
+      "items"
+    ])
+  }
+</script>
