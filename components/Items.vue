aa0ff618025d8f00600565fde3eb49dd5dc71934
Mhelvin Reyes <mhelvinreyes26@gmail.com>
2017-11-29 14:30:11 +0800

    Style a List and List Items in Vue.js and Nuxt

--- components/Items.vue
+++ components/Items.vue
@@ -1,7 +1,20 @@
 <template>
   <div class="code">
     <ul>
-      <li v-for="item in items" :key="item.id">{{item.title}}</li>
+      <li class="item" v-for="item in items" :key="item.id">
+        <div class="score">
+          {{item.score}}
+        </div>
+        <div class="title">
+          {{item.title}}
+        </div>
+        <div class="details">
+          by {{item.by}} {{item.time}}
+        </div>
+        <div class="comments">
+          {{item.descendants}} comments
+        </div>
+      </li>
     </ul>
   </div>
 </template>
@@ -14,3 +27,32 @@
     ])
   }
 </script>
+
+<style scoped>
+  .item {
+    display: grid;
+    grid: repeat(4, 1.5em) / repeat(10, 1fr);
+    grid-row-gap: 1em;
+  }
+  .score {
+    grid-row: 1 / -1;
+    grid-column: span 1;
+    align-self: center;
+    justify-content: center;
+  }
+  .title {
+    grid-row: 1 /3;
+    grid-column: 2 / -2;
+    align-self: end;
+  }
+  .details {
+    grid-row: 3 / -1;
+    grid-column: 6 / -2;
+  }
+  .comments {
+    grid-row: 3 / -1;
+    grid-column: 2 / 6;
+    justify-content: end;
+  }
+</style>
+
