5728a3b1e5020d7345c0c2e52f823ddd6f6af303
Mhelvin Reyes <mhelvinreyes26@gmail.com>
2017-12-01 12:16:49 +0800

    Display a User Data Page Based on id in Vue.js and Nuxt

--- components/Items.vue
+++ components/Items.vue
@@ -13,7 +13,7 @@
           </template>
         </div>
         <div class="details">
-          by {{item.by}} 
+          by <nuxt-link :to="`/user/${item.by}`">{{item.by}}</nuxt-link>
           <p class="ma0 i f7">{{item.time | timeSince}} ago</p>
         </div>   
         <template v-if="item.descendants">
